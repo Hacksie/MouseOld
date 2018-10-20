5fdd64434dd6360e813e55b3d2c0bcab4d8674ff
Unknown <hackerxse@hotmail.com>
2018-10-20 11:49:18 +1000

    Select menu updates

--- Assets/Scripts/SelectMenuManager.cs
+++ Assets/Scripts/SelectMenuManager.cs
@@ -6,8 +6,27 @@ using UnityEngine.SceneManagement;
 namespace HackedDesign {
 	public class SelectMenuManager : MonoBehaviour {
 
+		public SelectMenuState state = SelectMenuState.INFO;
+
+		public void SetMenuState(SelectMenuState state)
+		{
+			this.state = state;
+		}
+
+		public SelectMenuState GetMenuState()
+		{
+			return state;
+		}
+
 		public void ResumeEvent () {
 			CoreGame.instance.SetResume ();
+		}
+
+		public enum SelectMenuState {
+			INFO,
+			TASKS,
+			STASH,
+			PSYCH
 		}
 	}
 }
--- Assets/Scripts/SelectMenuPanelPresenter.cs
+++ Assets/Scripts/SelectMenuPanelPresenter.cs
@@ -7,6 +7,12 @@ namespace HackedDesign {
 
 		SelectMenuManager selectMenuManager;
 
+		// Inject these in
+		public GameObject InfoPanel;
+		public GameObject TaskPanel;
+		public GameObject StashPanel;
+		public GameObject PsychPanel;
+
 
 		public void Repaint () {
 			Debug.Log("Repaint select");
@@ -14,12 +20,65 @@ namespace HackedDesign {
 				this.gameObject.SetActive (true);
 			} else {
 				this.gameObject.SetActive (false);
+				return;
 			}
 
+			HideAll();
+
+			switch(selectMenuManager.GetMenuState())
+			{
+				case SelectMenuManager.SelectMenuState.INFO:
+				break;
+
+				case SelectMenuManager.SelectMenuState.TASKS:
+				ShowTasks();
+				break;
+
+				case SelectMenuManager.SelectMenuState.STASH:
+				break;
+
+				case SelectMenuManager.SelectMenuState.PSYCH:
+				break;
+			}
 		}
 
-		void Show () {
+		public void HideAll()
+		{
+			if(InfoPanel != null) {
+				InfoPanel.SetActive(false);
+			}
 
+			if(TaskPanel != null) {
+				TaskPanel.SetActive(false);
+			}
+			
+			if(StashPanel != null) {
+				StashPanel.SetActive(false);
+			}
+
+			if(PsychPanel != null) {
+				PsychPanel.SetActive(false);
+			}
+		}
+
+		public void ShowTasks()
+		{
+			TaskPanel.SetActive(true);
+		}
+
+		
+		public void InfoClickEvent()
+		{
+			Debug.Log("Select Menu Info Clicked");
+			selectMenuManager.SetMenuState(SelectMenuManager.SelectMenuState.INFO);
+			Repaint();
+		}
+
+		public void TaskClickEvent()
+		{
+			Debug.Log("Select Menu Task Clicked");
+			selectMenuManager.SetMenuState(SelectMenuManager.SelectMenuState.TASKS);
+			Repaint();
 		}
 
 		public void Initialize (SelectMenuManager selectMenuManager) {
