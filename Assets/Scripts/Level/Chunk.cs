55b35b9d26699bbf63f257e1548bf1dd05c6756e
Unknown <hackerxse@hotmail.com>
2018-11-06 23:51:46 +1000

    Beginning work on roguelike structures

---
+++ Assets/Scripts/Level/Chunk.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HackedDesign {
+	namespace Level {
+		public class Chunk : ScriptableObject {
+
+			public bool[] open = new bool[4];
+			public bool[] door = new bool[4];
+
+			public GameObject prefab;
+
+			public ColorPalette colorPalette;
+
+
+			public enum ColorPalette {
+				Magenta,
+				Blue,
+				Cyan,
+				Beige
+			}
+		}
+	}
+}
